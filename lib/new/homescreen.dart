7706768ff22f95907296a306da625bd3966d63cf
niky <kinycx@gmail.com>
2020-12-13 18:30:59 +0100

    -Codice fine secondo video -scollview orizzontale -categoria cibo con informazioni dinamiche

--- lib/new/homescreen.dart
+++ lib/new/homescreen.dart
@@ -1,4 +1,6 @@
+import 'package:cast/new/widgets/food_category.dart';
 import 'package:flutter/material.dart';
+import 'widgets/home_top_info.dart';
 
 class HomeScreen extends StatefulWidget{
   @override
@@ -8,28 +10,17 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreenState extends State<HomeScreen>{
 
-  final textStyle = TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold);
+
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       body: ListView(
-        padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+        padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text("Cosa vorresti", style: textStyle),
-                  Text("mangiare?", style: textStyle,)
-                ],
-              ),
-              Icon(Icons.notifications_none, size:30.0, color: Theme.of(context).primaryColor,),
-            ],
-          )
+          HomeTopInfo(),
+          FoodCategory(),
         ],
       ),
     );
